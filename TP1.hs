e03485cee86801c9562e79a5cb8a968e1b4bafaa
Pedro Marinho <pmiguel.marinho@gmail.com>
2024-10-11 20:17:36 +0100

    SCC exercise done two more and we good

--- TP1.hs
+++ TP1.hs
@@ -23,7 +23,7 @@ distance graph city1 city2 = Data.Maybe.listToMaybe edges
                             where edges = [ distance |(city1',city2',distance)<-graph, (city1' == city1 && city2' == city2) || (city1' == city2 && city2' == city1)]
 
 adjacent :: RoadMap -> City -> [(City,Distance)]
-adjacent graph city = [if city == city1 then (city2,distance) else (city1,distance)  |(city1,city2,distance)<-graph, city == city1 || city == city2 ] 
+adjacent graph city = [if city == city1 then (city2,distance) else (city1,distance) |(city1,city2,distance)<-graph, city == city1 || city == city2 ] 
 
 pathDistance :: RoadMap -> Path -> Maybe Distance
 pathDistance graph path = if all (==True) [areAdjacent graph (path !! index) (path !! (index + 1)) | index<-[0..length path - 2]] then fmap sum $ sequence [ distance graph (path !! index) (path !! (index + 1))| index<-[0..length path - 2]] else Nothing
@@ -33,8 +33,16 @@ rome graph = [city | (city,nAdj)<- city_adj, nAdj == maxAdj]
             where city_adj = [(city, length (adjacent graph city))| city <-cities graph]
                   maxAdj = maximum (map snd city_adj)
 
+
+neighbors :: RoadMap -> City -> [City]
+neighbors graph node = [city |(city,_)<-(adjacent graph node)]
+
+dfs graph node visited | node `elem` visited = visited -- If the city as already been visited return the visited nodes
+                       | otherwise = foldl (\acc adj -> dfs graph adj acc) (node : visited) (neighbors graph node) -- here we traverse the adj nodes while storing them recursively in the acc
+
+
 isStronglyConnected :: RoadMap -> Bool
-isStronglyConnected  = undefined
+isStronglyConnected  graph = length (cities graph) == length (dfs graph "0" []) -- Here im assuming "0" is in all graphs
 
 shortestPath :: RoadMap -> City -> City -> [Path]
 shortestPath = undefined
@@ -55,3 +63,8 @@ gTest2 = [("0","1",10),("0","2",15),("0","3",20),("1","2",35),("1","3",25),("2",
 
 gTest3 :: RoadMap -- unconnected graph
 gTest3 = [("0","1",4),("2","3",2)]
+
+-- More test graphs
+gTest4 :: RoadMap
+gTest4 =  [("1", "0", 1), ("2", "3", 1), ("5", "6", 1), ("6", "1", 1)]
+
