872043d2a2e20eeda2c06e524abef927a1b35c7d
Pedro Marinho <pmiguel.marinho@gmail.com>
2024-10-19 16:30:14 +0100

    Corrected code and added some documentation

--- TP1.hs
+++ TP1.hs
@@ -1,7 +1,7 @@
 import qualified Data.List
 import qualified Data.Array
 import qualified Data.Bits
-import Debug.Trace (trace)
+import Debug.Trace (trace) -- retirar no final (bom para debug)
 -- PFL 2024/2025 Practical assignment 1
 
 -- | Type alias for City, representing a city's name as a string.
@@ -152,15 +152,38 @@ dfs graph node visited | node `elem` visited = visited -- If the city as already
 isStronglyConnected :: RoadMap -> Bool
 isStronglyConnected  graph = length (cities graph) == length (dfs graph "0" []) -- Here im assuming "0" is in all graphs (if not can change later )
 
-
 type PriorityQueue = [(City,Distance)]
+
+-- | Function: getMinPrioQueue
+-- | Goal: Retrieves the city with the minimum distance from the priority queue.
+-- | Arguments:
+-- |    prioQueue: A PriorityQueue, which is a list of tuples containing a city and its distance.
+-- | Returns: A tuple (City, Distance) representing the city with the smallest distance in the priority queue.
+-- | Time Complexity: O(n), where n is the number of elements in the priority queue.
 
 getMinPrioQueue :: PriorityQueue -> (City, Distance)
 getMinPrioQueue (x:xs) = foldl (\acc@(city1, dist1) (city2, dist2) -> if dist2 < dist1 then (city2, dist2) else acc) x xs
 
+-- | Function: getMinPrioQueue
+-- | Goal: Retrieves the city with the minimum distance from the priority queue.
+-- | Arguments:
+-- |    prioQueue: A PriorityQueue, which is a list of tuples containing a city and its distance.
+-- | Returns: A tuple (City, Distance) representing the city with the smallest distance in the priority queue.
+-- | Time Complexity: O(n), where n is the number of elements in the priority queue.
+
+popMinPriorityQueue :: PriorityQueue ->  (City, Distance) -> PriorityQueue
 popMinPriorityQueue prioQueue min = filter (\(city, _) -> city /= fst min) prioQueue
 
--- Function to get unvisited adjacent nodes from the adjacency list
+
+-- | Function: nodesNotVisited
+-- | Goal: Retrieves a list of unvisited adjacent nodes from the adjacency list of a given city.
+-- | Arguments:
+-- |    adjList: The adjacency list representing the graph structure.
+-- |    minCity: The city whose neighbors are being checked.
+-- |    visited: A list of cities that have already been visited.
+-- | Returns: A list of tuples (City, Distance) representing the unvisited neighbors of the city.
+-- | Time Complexity: O(k), where k is the number of neighbors of the city .
+
 nodesNotVisited :: AdjList -> City -> [City] -> [(City, Distance)]
 nodesNotVisited adjList minCity visited = 
     case lookup minCity adjList of
@@ -168,19 +191,28 @@ nodesNotVisited adjList minCity visited =
             filter (\(city, _) -> city `notElem` visited) neighbors  -- Keep only unvisited neighbors
         Nothing -> []  -- Return empty list if node has no edges
 
+-- | Function: dijkstra
+-- | Goal: Implements Dijkstra's algorithm to find the shortest path from the source to all other cities in the graph.
+-- | Arguments:
+-- |    adjList: The adjacency list representing the graph structure.
+-- |    visited: A list of cities that have already been visited.
+-- |    predecessors: A list of tuples (City, [City]) representing the predecessors for each city.
+-- |    initialDistances: A list of tuples (City, Distance) representing the current known shortest distance to each city.
+-- |    initialPrioQueue: A PriorityQueue of cities ordered by their current known shortest distance.
+-- | Returns: A list of tuples (City, [City]) representing the predecessors for each city after completing Dijkstra's algorithm.
+-- | Time Complexity: O((V + E) * log V), where V is the number of cities and E is the number of roads.
 
 dijkstra :: AdjList -> [City] -> [(City, [City])] -> [(City, Distance)] -> [(City, Distance)] -> [(City, [City])]
 dijkstra adjList visited predecessors initialDistances initialPrioQueue 
     | null initialPrioQueue = predecessors  -- Base case: no more nodes to process
-    | otherwise = trace ("Processing: " ++ show (fst min) ++ ", Distance: " ++ show (snd min) ++ ", Priority Queue: " ++ show initialPrioQueue) $
+    | otherwise = 
                   dijkstra adjList newVisited newPredecessors newDistances updatedPrioQueue  -- Recursive call
     where 
         min = getMinPrioQueue initialPrioQueue  -- Get the city with the minimum distance
         newPrioQueue = popMinPriorityQueue initialPrioQueue min  -- Remove it from the priority queue
         newVisited = visited ++ [fst min]  -- Add the city to the visited list
         unvisitedNodes = nodesNotVisited adjList (fst min) visited  -- Get unvisited neighbors of the current city
-
-        -- Update distances and predecessors, and prepare the updated priority queue
+        -- Update distances and predecessors, and prepare to updated priority queue
         (newDistances, newPredecessors, updatedPrioQueue) = foldl updateDistances (initialDistances, predecessors, newPrioQueue) unvisitedNodes
             where
                 updateDistances (distances, preds, prioQueue) (neighborCity, weight) =
@@ -188,31 +220,31 @@ dijkstra adjList visited predecessors initialDistances initialPrioQueue
                         newDistance = currentDistance + weight  -- New distance through the current node
                         existingDistance = lookupDistance neighborCity distances  -- Existing distance to the neighbor
                         existingPreds = snd (head $ filter ((== neighborCity) . fst) preds)  -- Get existing predecessors for the neighbor
-                    in trace ("Checking neighbor: " ++ neighborCity ++ ", New Distance: " ++ show newDistance ++ ", Existing Distance: " ++ show existingDistance) $
+                    in  
                        case compare newDistance existingDistance of
                         LT ->  -- If the new distance is less
                             let updatedDistances = (neighborCity, newDistance) : filter ((/= neighborCity) . fst) distances
-                                updatedPreds = preds ++ [(neighborCity, [fst min])]  -- Add current node to predecessors
-                            in trace ("Updated distances: " ++ show updatedDistances ++ ", Updated predecessors: " ++ show updatedPreds) $
+                                updatedPreds = (neighborCity, [fst min]) : filter ((/= neighborCity) . fst) preds  -- Replace the predecessors list
+                            in 
                                (updatedDistances, updatedPreds, insertIntoPriorityQueue (neighborCity, newDistance) prioQueue)  -- Add to priority queue
                         EQ ->  -- If the distances are equal
                                    if fst min `notElem` existingPreds  -- Check if the current city is already a predecessor
                                         then
                                             let updatedPreds = (neighborCity, existingPreds ++ [fst min]) : filter ((/= neighborCity) . fst) preds  -- Append current city to predecessors
-                                            in trace ("Equal distances for " ++ neighborCity ++ ", Updated predecessors: " ++ show updatedPreds) $
+                                            in 
                                             (distances, updatedPreds, prioQueue)  -- No update needed for distance
                                         else 
-                                            trace ("No update needed for " ++ neighborCity) $
+                                            
                                             (distances, preds, prioQueue)  -- No update needed
                         GT ->  -- If the new distance is greater
                             if existingDistance == 100000000  -- Check if the existing distance is infinite
                             then 
                                 let updatedDistances = (neighborCity, newDistance) : filter ((/= neighborCity) . fst) distances
                                     updatedPreds = preds ++ [(neighborCity, [fst min])]  -- Add current node to predecessors
-                                in trace ("Existing distance was infinite for " ++ neighborCity ++ ", Updated distances: " ++ show updatedDistances ++ ", Updated predecessors: " ++ show updatedPreds) $
+                                in 
                                    (updatedDistances, updatedPreds, insertIntoPriorityQueue (neighborCity, newDistance) prioQueue)  -- Add to priority queue
                             else 
-                                trace ("No updates needed for " ++ neighborCity) $
+                                
                                 (distances, preds, prioQueue)  -- No updates needed
 
                 lookupDistance city distances = case lookup city distances of
@@ -222,28 +254,49 @@ dijkstra adjList visited predecessors initialDistances initialPrioQueue
                 -- Function to insert the city with its distance into the priority queue
                 insertIntoPriorityQueue (city, distance) prioQueue = (city, distance) : prioQueue  -- Add new city with its distance to the priority queue
 
-getPredecessor :: [(City, [City])] -> City -> [City]
-getPredecessor [] _ = []
-getPredecessor ((city, predecessors):rest) targetCity
+
+-- | Function: lookupPredecessors
+-- | Goal: Retrieve the list of predecessor cities for a given target city.
+-- | Arguments:
+-- |    predecessorsList: A list of tuples (City, [City]) where each city is mapped to its predecessor cities.
+-- |    targetCity: The city for which we want to retrieve the predecessors.
+-- | Returns: A list of predecessor cities for the given target city.
+-- | Time Complexity: O(N), where N is the number of cities in the predecessorsList.
+
+lookupPredecessors :: [(City, [City])] -> City -> [City]
+lookupPredecessors [] _ = []
+lookupPredecessors ((city, predecessors):rest) targetCity
     | city == targetCity = predecessors
-    | otherwise = getPredecessor rest targetCity
+    | otherwise = lookupPredecessors rest targetCity
 
+-- | Function: collectAllPaths
+-- | Goal: Recursively find all possible paths from a target city to the source city using the predecessors.
+-- | Arguments:
+-- |    predecessorsList: A list of tuples (City, [City]) representing predecessors of each city.
+-- |    targetCity: The city from which we start the pathfinding (the destination).
+-- |    currPath: The current path being built (starting from the target city).
+-- | Returns: A list of all possible paths from the target city to the source city.
+-- | Time Complexity: O(P), where P is the number of possible paths from the target city to the source.
 
-
--- Recursively finds all possible paths from the target city to the source using the city predecessors.
-findPathsFromCity :: [(City, [City])] -> City -> Path -> [Path]
-findPathsFromCity cityPredecessors targetCity currPath
-    | null predecessors = [currPath]    -- Reached the initial source city (predecessors of that city is an empty list)
-    | otherwise = foldl (\pathsAcc predecessor -> findPathsFromCity cityPredecessors predecessor (predecessor : currPath) ++ pathsAcc ) [] predecessors
+collectAllPaths :: [(City, [City])] -> City -> Path -> [Path]
+collectAllPaths predecessorsList targetCity currPath
+    | null previousCities = [currPath]    -- Reached the initial source city (predecessors of that city is an empty list)
+    | otherwise = foldl (\pathsAcc predecessor -> collectAllPaths predecessorsList predecessor (predecessor : currPath) ++ pathsAcc ) [] previousCities
     where
-        predecessors = getPredecessor cityPredecessors targetCity
+        previousCities = lookupPredecessors predecessorsList targetCity
         
-
-
-shortestPath :: RoadMap -> City -> City -> [Path] -- Utilizar dikstra
+-- | Function: shortestPath
+-- | Goal: Finds all the shortest paths from the start city to the end city in a roadmap using Dijkstra's algorithm.
+-- | Arguments:
+-- |    graph: A RoadMap, which is a list of tuples (City, City, Distance), representing connections between cities.
+-- |    start: The starting city.
+-- |    end: The destination city.
+-- | Returns: A list of paths, where each path is a list of cities, representing all possible shortest paths from the start to the end city.
+-- | Time Complexity: O((V + E) log V), where V is the number of cities and E is the number of roads. The log V factor comes from the priority queue used in Dijkstra's algorithm.
+shortestPath :: RoadMap -> City -> City -> [Path] -- Utilizes dikstra
 shortestPath graph start end | start == end = [[start]]
                              | not(end `elem` dfs graph start []) = [[]]
-                             | otherwise = findPathsFromCity (Debug.Trace.trace ("Auxiliary result from Dijkstra: " ++ show aux) aux) end [end]
+                             | otherwise = collectAllPaths (Debug.Trace.trace ("Auxiliary result from Dijkstra: " ++ show aux) aux) end [end]
                               where adjList = convertToAdjList graph
                                     initialPrio = [(start,0)]
                                     initialDistances = foldl (\acc city -> (if city == start then (city,0) else (city,100000000)) : acc) [] (cities graph)
@@ -274,7 +327,7 @@ gTest4 :: RoadMap
 gTest4 =  [("1", "0", 1), ("2", "3", 1), ("5", "6", 1), ("6", "1", 1)]
 
 gTest5 :: RoadMap
-gTest5 = [("1", "2", 1), ("2", "3", 1), ("1", "4", 1), ("4", "3", 1)]
+gTest5 = [("1", "2", 1), ("2", "3", 1), ("1", "4", 1), ("4", "3", 1),("1","3",100)]
 
 gTest6 :: RoadMap
 gTest6 = [("1", "2", 1), 
@@ -284,4 +337,14 @@ gTest6 = [("1", "2", 1),
          ("2", "5", 2), 
          ("3", "5", 2), 
          ("4", "5", 1)]
+
+gTest7 :: RoadMap
+gTest7 = [("0","2",5),("0","3",3),("3","2",1),("3","1",2),("2","1",2),("2","4",2),("1","4",1),("0","4",200)]
+
+
+gTest8 :: RoadMap
+gTest8 = [("0", "1", 6), ("0", "2", 6), ("0", "3", 46), ("0", "4", 31), ("0", "5", 51), ("0", "6", 70), ("0", "7", 18), ("0", "8", 29), ("0", "9", 63), ("0", "10", 18), ("0", "11", 78), ("0", "12", 69), ("0", "13", 43), ("0", "14", 36), ("0", "15", 6), ("0", "16", 51), ("0", "17", 0), ("0", "18", 65), ("0", "19", 86), ("1", "2", 25), ("1", "3", 87), ("1", "4", 99), ("1", "5", 40), ("1", "6", 82), ("1", "7", 61), ("1", "8", 29), ("1", "9", 18), ("1", "10", 91), ("1", "11", 58), ("1", "12", 31), ("1", "13", 91), ("1", "14", 56), ("1", "15", 45), ("1", "16", 42), ("1", "17", 27), ("1", "18", 91), ("1", "19", 6), ("2", "3", 15), ("2", "4", 15), ("2", "5", 43), ("2", "6", 63), ("2", "7", 29), ("2", "8", 18), ("2", "9", 63), ("2", "10", 77), ("2", "11", 35), ("2", "12", 90), ("2", "13", 40), ("2", "14", 71), ("2", "15", 45), ("2", "16", 65), ("2", "17", 82), ("2", "18", 39), ("2", "19", 19), ("3", "4", 69), ("3", "5", 72), ("3", "6", 49), ("3", "7", 89), ("3", "8", 6), ("3", "9", 72), ("3", "10", 45), ("3", "11", 52), ("3", "12", 46), ("3", "13", 7), ("3", "14", 80), ("3", "15", 99), ("3", "16", 98), ("3", "17", 92), ("3", "18", 77), ("3", "19", 2), ("4", "5", 3), ("4", "6", 24), ("4", "7", 57), ("4", "8", 95), ("4", "9", 59), ("4", "10", 41), ("4", "11", 80), ("4", "12", 0), ("4", "13", 5), ("4", "14", 25), ("4", "15", 48), ("4", "16", 13), ("4", "17", 57), ("4", "18", 61), ("4", "19", 99), ("5", "6", 21), ("5", "7", 75), ("5", "8", 33), ("5", "9", 83), ("5", "10", 92), ("5", "11", 59), ("5", "12", 80), ("5", "13", 25), ("5", "14", 8), ("5", "15", 73), ("5", "16", 19), ("5", "17", 9), ("5", "18", 84), ("5", "19", 68), ("6", "7", 91), ("6", "8", 10), ("6", "9", 27), ("6", "10", 92), ("6", "11", 49), ("6", "12", 54), ("6", "13", 84), ("6", "14", 79), ("6", "15", 5), ("6", "16", 14), ("6", "17", 87), ("6", "18", 66), ("6", "19", 53), ("7", "8", 47), ("7", "9", 1), ("7", "10", 78), ("7", "11", 53), ("7", "12", 79), ("7", "13", 94), ("7", "14", 99), ("7", "15", 14), ("7", "16", 47), ("7", "17", 90), ("7", "18", 15), ("7", "19", 46), ("8", "9", 78), ("8", "10", 69), ("8", "11", 42), ("8", "12", 66), ("8", "13", 95), ("8", "14", 10), ("8", "15", 37), ("8", "16", 89), ("8", "17", 73), ("8", "18", 83), ("8", "19", 47), ("9", "10", 20), ("9", "11", 42), ("9", "12", 63), ("9", "13", 19), ("9", "14", 83), ("9", "15", 6), ("9", "16", 62), ("9", "17", 53), ("9", "18", 42), ("9", "19", 52), ("10", "11", 84), ("10", "12", 52), ("10", "13", 30), ("10", "14", 14), ("10", "15", 27), ("10", "16", 58), ("10", "17", 76), ("10", "18", 31), ("10", "19", 5), ("11", "12", 0), ("11", "13", 61), ("11", "14", 49), ("11", "15", 13), ("11", "16", 65), ("11", "17", 24), ("11", "18", 70), ("11", "19", 90), ("12", "13", 70), ("12", "14", 60), ("12", "15", 25), ("12", "16", 81), ("12", "17", 99), ("12", "18", 51), ("12", "19", 29), ("13", "14", 65), ("13", "15", 63), ("13", "16", 28), ("13", "17", 30), ("13", "18", 57), ("13", "19", 23), ("14", "15", 63), ("14", "16", 81), ("14", "17", 4), ("14", "18", 66), ("14", "19", 40), ("15", "16", 48), ("15", "17", 82), ("15", "18", 72), ("15", "19", 28), ("16", "17", 17), ("16", "18", 30), ("16", "19", 51), ("17", "18", 99), ("17", "19", 34), ("18", "19", 29)]
+
+gTest9 :: RoadMap
+gTest9 = [("0","1",4),("0","2",1),("2","3",1), ("3","4",1), ("4", "1", 1), ("0","5",2),("5","1",2)]
 
